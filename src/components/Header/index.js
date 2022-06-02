51fe78137637bf4134cdda7ba24a3c75ed2e4aa8
Hajara <hajaraiyal@gmail.com>
2022-06-02 22:59:34 +0100

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ function Header() {
 	return (
 		<div className="header">
 			<h3>Hey Hajara,</h3>
-			<h5>Let's set some goals for 2022!</h5>
+			<h5>Let us set some goals for 2022!</h5>
 		</div>
 	);
 }
